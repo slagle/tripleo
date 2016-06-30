20d51c6e6a9ef0d44fceb582c33f68ff724215cf
James Slagle <jslagle@redhat.com>
2016-06-30 16:14:38 -0400

    Update external net subnet

--- scripts/external-network-vlan.sh
+++ scripts/external-network-vlan.sh
@@ -3,5 +3,5 @@
 set -ux
 
 sudo ovs-vsctl add-port br-ctlplane vlan10 tag=10 -- set interface vlan10 type=internal
-sudo ip l set dev vlan10 up; sudo ip addr add 192.0.3.251/24 dev vlan10
-sudo iptables -A BOOTSTACK_MASQ -s 192.0.3.0/24 ! -d 192.0.3.0/24 -j MASQUERADE -t nat
+sudo ip l set dev vlan10 up; sudo ip addr add 10.0.0.1/24 dev vlan10
+sudo iptables -A BOOTSTACK_MASQ -s 10.0.0.0/24 ! -d 10.0.0.0/24 -j MASQUERADE -t nat
