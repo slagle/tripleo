4a4588347e5d05f8c33a7fcd9012d374cbc4be62
James Slagle <jslagle@redhat.com>
2016-06-30 09:12:51 -0400

    Add external-network-vlan.sh

---
+++ external-network-vlan.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -ux
+
+sudo ovs-vsctl add-port br-ctlplane vlan10 tag=10 -- set interface vlan10 type=internal
+sudo ip l set dev vlan10 up; sudo ip addr add 192.0.3.251/24 dev vlan10
+sudo iptables -A BOOTSTACK_MASQ -s 192.0.3.0/24 ! -d 192.0.3.0/24 -j MASQUERADE -t nat
