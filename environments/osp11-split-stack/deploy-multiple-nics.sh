1e4ae5b069e99cc084240f3e800605aa322508f6
James Slagle <jslagle@redhat.com>
2017-04-12 09:31:42 -0400

    Add network-isolation for deployed-server

--- environments/osp11-split-stack/deploy-multiple-nics.sh
+++ environments/osp11-split-stack/deploy-multiple-nics.sh
@@ -9,7 +9,7 @@ openstack overcloud deploy \
   -e /usr/share/openstack-tripleo-heat-templates/environments/deployed-server-environment.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/deployed-server-bootstrap-environment-rhel.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/puppet-pacemaker.yaml \
-  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+  -e newtork-isolation-deployed-server.yaml \
   -e network-environment-multiple-nics.yaml \
   -e role-counts.yaml \
   -r deployed-server-roles-data.yaml
