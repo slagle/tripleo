a76d9e3f3c10ee8f25574e73253823ae3e2c24a8
James Slagle <jslagle@redhat.com>
2017-07-26 09:27:43 -0400

    cinder-standalone

---
+++ environments/cinder-standalone/deploy-cinder-standalone.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eux
+
+export HEATCLIENT=1
+export STACK_NAME=cinder-standalone
+export ROLES_DATA=$TEMPLATES/roles_data_undercloud.yaml
+export COMMON_ENVIRONMENTS=0
+
+export ENVIRONMENTS="\
+    -e $TEMPLATES/environments/docker.yaml \
+    -e cinder-standalone.yaml"
+
+deploy.sh
---
+++ scripts/deploy.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -eux
+
+STACK_NAME=${STACK_NAME:-"overcloud"}
+UPDATE=${UPDATE:-"0"}
+HEATCLIENT=${HEATCLIENT:-"0"}
+STACK_OP=${STACK_OP:-"create"}
+TEMPLATES=${TEMPLATES:-"tripleo-heat-templates"}
+ROLES_DATA=${ROLES_DATA:-"$(realpath roles_data.yaml)"}
+COMMON_ENVIRONMENTS=${COMMON_ENVIRONMENTS:-"1"}
+ENVIRONMENTS=${ENVIRONMENTS:-""}
+
+if [ "$COMMON_ENVIRONMENTS" = "1" ]; then
+    ENVIRONMENTS="-e $TEMPLATES/overcloud-resource-registry-puppet.yaml $ENVIRONMENTS"
+    ENVIRONMENTS="-e $TEMPLATES/environments/puppet-pacemaker.yaml $ENVIRONMENTS"
+fi
+
+if [ "$UPDATE" = "1" ]; then
+	STACK_OP="update"
+fi
+
+# sudo systemctl restart openstack-zaqar@1 openstack-mistral-*
+
+find tripleo-heat-templates | grep 'j2\.' | sed 's/j2\.//' | xargs -rtn1 rm -f
+
+if [ "$HEATCLIENT" = "0" ]; then
+
+        if [ "$UPDATE" = "0" ]; then
+                if mistral environment-get $STACK_NAME; then
+                    mistral environment-delete $STACK_NAME
+                fi
+
+                if swift stat $STACK_NAME; then
+                    swift delete $STACK_NAME
+                fi
+
+                if swift stat ${STACK_NAME}-swift-rings; then
+                    swift delete ${STACK_NAME}-swift-rings
+                fi
+        fi
+
+	time openstack overcloud deploy \
+		--stack $STACK_NAME \
+		--skip-deploy-identifier \
+		--disable-validations \
+		--templates $TEMPLATES \
+		-r $ROLES_DATA \
+		$ENVIRONMENTS \
+        $@
+else
+	pushd $TEMPLATES
+	tools/process-templates.py -r $ROLES_DATA
+	popd
+
+	time openstack stack $STACK_OP \
+		$STACK_NAME \
+		--wait \
+		--template $TEMPLATES/overcloud.yaml \
+		$ENVIRONMENTS \
+        $@
+fi
