69ad1a132de200eb7b70a03b7f6c3bd9b5dc93c2
James Slagle <jslagle@redhat.com>
2018-02-07 17:56:14 -0500

    instackenv

--- scripts/instackenv-to-vbmc.sh
+++ scripts/instackenv-to-vbmc.sh
@@ -2,8 +2,11 @@
 
 set -eux
 
-jq '.["nodes"][] | "vbmc add --username " + ."pm_user" + " --password " + ."pm_password" + " --port " + ."pm_port" + " --address " + ."pm_addr" + " --libvirt-uri qemu:///session " + (."name"|sub("-";"_"))' instackenv.json -r
-:xa
-ch
+sed -i s/192.168.24.1/192.168.122.1/g instackenv.json
 
-echo $(vbmc list | grep down | awk '{print $2}' | xargs -n 1 echo vbmc start)
+jq '.["nodes"][] | "vbmc add --username " + ."pm_user" + " --password " + ."pm_password" + " --port " + ."pm_port" + " --address " + ."pm_addr" + " --libvirt-uri qemu:///system " + (."name"|sub("-";"_"))' instackenv.json -r
+
+set +x
+for node in $(vbmc list | grep down | awk '{print $2}'); do
+  echo vbmc start $node
+done
