7cefc253967c21aa29ddae95b8e95bbfc135762f
James Slagle <jslagle@redhat.com>
2018-02-09 09:15:58 -0500

    updates

--- scripts/instackenv-to-vbmc.sh
+++ scripts/instackenv-to-vbmc.sh
@@ -2,9 +2,9 @@
 
 set -eux
 
-sed -i s/192.168.24.1/192.168.122.1/g instackenv.json
+sed -i s/192.168.24.1/192.168.122.1/g $1
 
-jq '.["nodes"][] | "vbmc add --username " + ."pm_user" + " --password " + ."pm_password" + " --port " + ."pm_port" + " --address " + ."pm_addr" + " --libvirt-uri qemu:///system " + (."name"|sub("-";"_"))' instackenv.json -r
+jq '.["nodes"][] | "vbmc add --username " + ."pm_user" + " --password " + ."pm_password" + " --port " + ."pm_port" + " --address " + ."pm_addr" + " --libvirt-uri qemu:///system " + (."name"|sub("-";"_"))' $1 -r
 
 set +x
 for node in $(vbmc list | grep down | awk '{print $2}'); do
