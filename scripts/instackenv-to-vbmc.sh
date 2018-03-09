7df53720c849a61722877d8d3d1131a7636abb95
James Slagle <jslagle@redhat.com>
2018-03-09 14:12:11 -0500

    updates

--- scripts/instackenv-to-vbmc.sh
+++ scripts/instackenv-to-vbmc.sh
@@ -2,8 +2,6 @@
 
 set -eux
 
-# sed -i s/192.168.24.1/192.168.122.1/g $1
-
 jq '.["nodes"][] | "sudo vbmc add --username " + ."pm_user" + " --password " + ."pm_password" + " --port " + ."pm_port" + " --address " + ."pm_addr" + " --libvirt-uri \"qemu+ssh://root@" + "192.168.23.1" + "/system?&keyfile=/root/.ssh/id_rsa_virt_power&no_verify=1&no_tty=1\" " + (."name"|sub("-";"_"))' $1 -r
 
 set +x
