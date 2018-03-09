1cd8efe453d3a5c7eb45a182a8a37c15a5fe6cb8
James Slagle <jslagle@redhat.com>
2018-03-09 14:10:55 -0500

    updates

--- scripts/instackenv-to-vbmc.sh
+++ scripts/instackenv-to-vbmc.sh
@@ -7,6 +7,6 @@ set -eux
 jq '.["nodes"][] | "sudo vbmc add --username " + ."pm_user" + " --password " + ."pm_password" + " --port " + ."pm_port" + " --address " + ."pm_addr" + " --libvirt-uri \"qemu+ssh://root@" + "192.168.23.1" + "/system?&keyfile=/root/.ssh/id_rsa_virt_power&no_verify=1&no_tty=1\" " + (."name"|sub("-";"_"))' $1 -r
 
 set +x
-for node in $(vbmc list | grep down | awk '{print $2}'); do
+for node in $(sudo vbmc list | grep down | awk '{print $2}'); do
   echo vbmc start $node
 done
