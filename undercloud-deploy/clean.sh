5d5bc1b2b7c425d72aed5d6b5ecf304c1ed74cc0
James Slagle <jslagle@redhat.com>
2018-07-10 10:23:21 -0400

    updates

--- undercloud-deploy/clean.sh
+++ undercloud-deploy/clean.sh
@@ -2,7 +2,7 @@
 
 set -ux
 
-sudo docker ps -q -a | xargs -t docker stop
-sudo docker ps -q -a | xargs -t docker rm -f
+sudo docker ps -q -a | sudo xargs -t docker stop
+sudo docker ps -q -a | sudo xargs -t docker rm -f
 
 sudo rm -rf /var/lib/mysql/* /var/lib/rabbitmq/* /var/lib/config-data/* /var/lib/heat-config/* /var/log/heat* /var/lib/docker-puppet /var/lib/docker-config-scripts /var/lib/ironic/* /var/lib/ironic-inspector/*
