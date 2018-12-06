29cc850d9362d3532f5d14be6cb4c9baac3363d9
James Slagle <jslagle@redhat.com>
2018-12-05 19:41:15 -0500

    updates

---
+++ environments/split-controlplane/export-controlplane.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -eux
+
+export OS_AUTH_TYPE=none
+export OS_ENDPOINT=http://127.0.0.1:8006/v1/admin
+export DIR=export-control-plane
+export STACK=${STACK:-"controlplane"}
+
+mkdir -p $DIR
+
+openstack stack output show $STACK EndpointMap --format json \
+  | jq '{"parameter_defaults": {"EndpointMapOverride": .output_value}}' \
+  > $DIR/endpoint-map.json
+
+openstack stack output show $STACK AllNodesConfig --format json \
+  | jq '{"parameter_defaults": {"AllNodesExtraMapData": .output_value}}' \
+  > $DIR/all-nodes-extra-map-data.json
+
+openstack stack output show $STACK HostsEntry -f json \
+  | jq -r '{"parameter_defaults":{"ExtraHostFileEntries": .output_value}}' \
+  > $DIR/extra-host-file-entries.json
+
+cat >$DIR/oslo.yaml <<EOF
+parameter_defaults:
+  ComputeExtraConfig:
+    oslo_messaging_notify_use_ssl: false
+    oslo_messaging_rpc_use_ssl: false
+EOF
+
+sudo egrep "oslo.*password" /etc/puppet/hieradata/service_configs.json \
+  | sed -e s/\"//g -e s/,//g >> $DIR/oslo.yaml
+
+cp $HOME/tripleo-undercloud-passwords.yaml $DIR/passwords.yaml
+
+
+
