6436547d729304f23b9f3b57a353bbcfa6de1c03
James Slagle <jslagle@redhat.com>
2019-01-16 12:22:03 -0500

    updates

--- scripts/render-scale-templates.sh
+++ scripts/render-scale-templates.sh
@@ -3,6 +3,8 @@
 set -eux
 
 file=$(mktemp)
-sed -i "s/compute_count/$1" tripleo/environments/deployed-server-port-map.j2.yaml > $file
+sed "s/compute_count/$1/" tripleo/environments/deployed-server-port-map.j2.yaml > $file
 ansible -c local localhost -m template -a "src=$file dest=deployed-server-port-map.yaml"
-ansible -c local localhost -m template -a "src=tripleo/environments/hostname-map.j2.yaml dest=hostname-map.yaml"
+sed "s/compute_count/$1/" tripleo/environments/hostname-map.j2.yaml > $file
+ansible -c local localhost -m template -a "src=$file dest=hostname-map.yaml"
+rm -f $file
