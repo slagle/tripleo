68412024269e56443cbb18f747354dd4914345e5
James Slagle <jslagle@redhat.com>
2019-01-16 12:25:09 -0500

    updates

--- scripts/render-scale-templates.sh
+++ scripts/render-scale-templates.sh
@@ -8,3 +8,6 @@ ansible -c local localhost -m template -a "src=$file dest=deployed-server-port-m
 sed "s/compute_count/$1/" tripleo/environments/hostname-map.j2.yaml > $file
 ansible -c local localhost -m template -a "src=$file dest=hostname-map.yaml"
 rm -f $file
+
+cp tripleo/environments/role-counts.j2.yaml role-counts.yaml
+sed -i "s/compute_count/$1/" role-counts.yaml
