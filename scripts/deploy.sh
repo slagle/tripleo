fcee37482d3258a72415122d63fa441bf1b63555
James Slagle <jslagle@redhat.com>
2019-01-29 11:39:46 -0500

    updates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -60,14 +60,14 @@ if [ "$HEATCLIENT" = "0" ]; then
                 fi
         fi
 
-        time openstack overcloud deploy \
+        time unbuffer openstack overcloud deploy \
                 --verbose \
                 --stack $STACK_NAME \
                 --templates $TEMPLATES \
                 -r $ROLES_DATA \
                 $ARGS \
                 $ENVIRONMENTS \
-                $@
+                $@ 2>&1 | while read line; do date | tr "\n" " "; echo $line; done | unbuffer -p tee -a deploy.log
 else
         pushd $TEMPLATES
         tools/process-templates.py -r $ROLES_DATA
