ac6b27aa99f781ecf77e4c2edacd8aa14d420b91
James Slagle <jslagle@redhat.com>
2019-04-04 15:00:43 -0400

    updates

---
+++ environments/tripleo-0-dcn/port-map-tripleo-03.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -eux
+
+openstack baremetal node manage dcn1-compute-0
+openstack baremetal node manage dcn1-compute-1
+openstack baremetal node manage dcn1-compute-2
+
+uuid=$(openstack baremetal port list --node dcn1-compute-0 -f value -c Address)
+openstack baremetal port set --physical-network leaf1 $uuid
+
+uuid=$(openstack baremetal port list --node dcn1-compute-1 -f value -c Address)
+openstack baremetal port set --physical-network leaf1 $uuid
+
+uuid=$(openstack baremetal port list --node dcn1-compute-2 -f value -c Address)
+openstack baremetal port set --physical-network leaf1 $uuid
