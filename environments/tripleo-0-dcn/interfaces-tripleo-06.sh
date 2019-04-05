06e67a9acc7441cb635526489aa65d2e8fd50a9e
James Slagle <jslagle@redhat.com>
2019-04-05 10:07:48 -0400

    updates

--- environments/tripleo-0-dcn/interfaces-tripleo-06.sh
+++ environments/tripleo-0-dcn/interfaces-tripleo-06.sh
@@ -4,5 +4,5 @@ set -eux
 
 # InternalAPIDCN2 gateway
 sudo ip link add link dcn2-management name dcn2.1165 type vlan id 1165
-sudo ip a add 172.25.3.254/24 dev dcn1.1165
+sudo ip a add 172.25.3.254/24 dev dcn2.1165
 sudo ip link set dev dcn2.1165 up
