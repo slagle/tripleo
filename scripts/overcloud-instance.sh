ff699001b208b819e8b781867eed96425d56d0c2
James Slagle <jslagle@redhat.com>
2019-04-17 08:48:51 -0400

    updates

--- scripts/overcloud-instance.sh
+++ scripts/overcloud-instance.sh
@@ -13,7 +13,7 @@ fi
 
 if ! openstack keypair show overcloud-key; then
     openstack keypair create \
-        --private-key overcloud-key.rsa \
+        --public-key overcloud-key.rsa.pub \
         overcloud-key
 fi
 
