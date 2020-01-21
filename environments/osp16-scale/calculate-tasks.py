b61e33bee98cda08178dce5a827b532602843112
James Slagle <jslagle@redhat.com>
2020-01-20 20:22:58 -0500

    updates

---
+++ environments/osp16-scale/calculate-tasks.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import datetime
+import sys
+
+infile = sys.argv[1]
+
+play_start = task_start = play = task = None
+plays = {}
+tasks = {}
+
+with open(infile) as f:
+    line = f.readline()
+    if 'PLAY ' in line:
+        play = line
+        play_start = line.split(' ')[0]
+        if play_start:
+            play_length = play_end - play_start
+            plays[play] = play_length
+        play_start = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S,sss')
+    if 'TASK ' in line:
+        task = line
+        task_start = line.split(' ')[0]
+        if task_start:
+            task_length = task_end - task_start
+            tasks[task] = task_length
+        task-start = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S,sss')
+
+
