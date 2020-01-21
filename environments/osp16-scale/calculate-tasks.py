4f660b5ff2bfb8955978cf304b6a0a390d09254c
James Slagle <jslagle@redhat.com>
2020-01-20 20:38:22 -0500

    updates

--- environments/osp16-scale/calculate-tasks.py
+++ environments/osp16-scale/calculate-tasks.py
@@ -8,22 +8,25 @@ infile = sys.argv[1]
 play_start = task_start = play = task = None
 plays = {}
 tasks = {}
+old_start = {}
+old_start['PLAY '] = None
+old_start['TASK '] = None
 
 with open(infile) as f:
     line = f.readline()
     while line:
         for i in ['PLAY ', 'TASK ']:
             if i in line:
-                task = line
                 start = line.split(' ')[0] + ' ' + line.split(' ')[1].split(',')[0]
                 current = datetime.datetime.strptime(start, '%Y-%m-%d %H:%M:%S')
-                if task_start:
-                    length = current - task_start
+                if old_start[i]:
+                    length = current - old_start[i]
                     if 'PLAY' in i:
                         plays[task] = length
                     else:
                         tasks[task] = length
-                task_start = current
+                old_start[i] = current
+                task = line
         line = f.readline()
 
 for k, v in plays.items():
