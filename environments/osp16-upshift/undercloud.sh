fc90c95c74ae17c9a478e8ae50ac2b151e9141bb
James Slagle <jslagle@redhat.com>
2020-08-12 16:57:55 -0400

    updates

--- environments/osp16-upshift/undercloud.sh
+++ environments/osp16-upshift/undercloud.sh
@@ -2,7 +2,7 @@
 
 sudo dnf localinstall -y http://download.lab.bos.redhat.com/rcm-guest/puddles/OpenStack/rhos-release/rhos-release-latest.noarch.rpm
 sudo rhos-release 16.1
-sudo dnf install -y python3-tripleoclient git tmux expect
+sudo dnf install -y python3-tripleoclient git tmux expect bash-completion
 
 cat >~/.tmux.conf<<EOF
 set-option -g prefix2 C-a
@@ -33,3 +33,6 @@ EOF
 
 sudo ip link set dev eth2 up
 sudo ip addr add 10.0.0.5/24 dev eth2
+
+openstack complete | grep -v INFO | grep -v DEBUG | sudo tee /etc/bash_completion.d/openstack
+
