d615bf26e4813953d54f3d6c51a598f40ff8d443
James Slagle <jslagle@redhat.com>
2021-03-31 11:21:53 -0400

    updates

--- environments/master-upshift/commands.sh
+++ environments/master-upshift/commands.sh
@@ -1,89 +1,95 @@
 #!/bin/bash
 
-openstack network create --internal --disable-port-security jslagle-master-ctlplane &
-openstack network create --internal --disable-port-security jslagle-master-external &
-openstack network create --internal --disable-port-security jslagle-master-internalapi &
-openstack network create --internal --disable-port-security jslagle-master-storage &
-openstack network create --internal --disable-port-security jslagle-master-storagemgt &
-openstack network create --internal --disable-port-security jslagle-master-tenant &
+function create_networks {
+    openstack network create --internal --disable-port-security jslagle-master-ctlplane &
+    openstack network create --internal --disable-port-security jslagle-master-external &
+    openstack network create --internal --disable-port-security jslagle-master-internalapi &
+    openstack network create --internal --disable-port-security jslagle-master-storage &
+    openstack network create --internal --disable-port-security jslagle-master-storagemgt &
+    openstack network create --internal --disable-port-security jslagle-master-tenant &
+}
 
-openstack subnet create --gateway none --no-dhcp --network jslagle-master-ctlplane --subnet-range 192.168.24.0/24 jslagle-master-ctlplane &
-openstack subnet create --gateway none --no-dhcp --network jslagle-master-external --subnet-range 10.0.0.0/24 jslagle-master-external &
-openstack subnet create --gateway none --no-dhcp --network jslagle-master-internalapi --subnet-range 172.16.2.0/24 jslagle-master-internalapi &
-openstack subnet create --gateway none --no-dhcp --network jslagle-master-storage --subnet-range 172.16.1.0/24 jslagle-master-storage &
-openstack subnet create --gateway none --no-dhcp --network jslagle-master-storagemgt --subnet-range 172.16.3.0/24 jslagle-master-storagemgt &
-openstack subnet create --gateway none --no-dhcp --network jslagle-master-tenant --subnet-range 172.16.0.0/24 jslagle-master-tenant &
+function create_subnets {
+    openstack subnet create --gateway none --no-dhcp --network jslagle-master-ctlplane --subnet-range 192.168.24.0/24 jslagle-master-ctlplane &
+    openstack subnet create --gateway none --no-dhcp --network jslagle-master-external --subnet-range 10.0.0.0/24 jslagle-master-external &
+    openstack subnet create --gateway none --no-dhcp --network jslagle-master-internalapi --subnet-range 172.16.2.0/24 jslagle-master-internalapi &
+    openstack subnet create --gateway none --no-dhcp --network jslagle-master-storage --subnet-range 172.16.1.0/24 jslagle-master-storage &
+    openstack subnet create --gateway none --no-dhcp --network jslagle-master-storagemgt --subnet-range 172.16.3.0/24 jslagle-master-storagemgt &
+    openstack subnet create --gateway none --no-dhcp --network jslagle-master-tenant --subnet-range 172.16.0.0/24 jslagle-master-tenant &
+}
 
-openstack port create \
-    --fixed-ip ip-address=192.168.24.100 \
-    --network jslagle-master-ctlplane \
-    master-control-virtual-ip &
+function create_ports {
+    openstack port create \
+        --fixed-ip ip-address=192.168.24.100 \
+        --network jslagle-master-ctlplane \
+        master-control-virtual-ip &
 
-openstack port create \
-    --fixed-ip ip-address=172.16.2.100 \
-    --network jslagle-master-internalapi \
-    master-internalapi-virtual-ip &
+    openstack port create \
+        --fixed-ip ip-address=172.16.2.100 \
+        --network jslagle-master-internalapi \
+        master-internalapi-virtual-ip &
 
-openstack port create \
-    --fixed-ip ip-address=10.0.0.100 \
-    --network jslagle-master-external \
-    master-external-virtual-ip &
+    openstack port create \
+        --fixed-ip ip-address=10.0.0.100 \
+        --network jslagle-master-external \
+        master-external-virtual-ip &
 
-openstack port create \
-    --fixed-ip ip-address=172.16.1.100 \
-    --network jslagle-master-storage \
-    master-storage-virtual-ip &
+    openstack port create \
+        --fixed-ip ip-address=172.16.1.100 \
+        --network jslagle-master-storage \
+        master-storage-virtual-ip &
 
-openstack port create \
-    --fixed-ip ip-address=172.16.3.100 \
-    --network jslagle-master-storagemgt \
-    master-storagemgt-virtual-ip &
+    openstack port create \
+        --fixed-ip ip-address=172.16.3.100 \
+        --network jslagle-master-storagemgt \
+        master-storagemgt-virtual-ip &
 
-openstack port create \
-    --fixed-ip ip-address=172.16.2.101 \
-    --network jslagle-master-internalapi \
-    master-redis-virtual-ip &
+    openstack port create \
+        --fixed-ip ip-address=172.16.2.101 \
+        --network jslagle-master-internalapi \
+        master-redis-virtual-ip &
 
-openstack port create \
-    --fixed-ip ip-address=172.16.2.102 \
-    --network jslagle-master-internalapi \
-    master-ovndbs-virtual-ip &
+    openstack port create \
+        --fixed-ip ip-address=172.16.2.102 \
+        --network jslagle-master-internalapi \
+        master-ovndbs-virtual-ip &
 
-openstack port create \
-    --fixed-ip ip-address=192.168.24.2 \
-    --network jslagle-master-ctlplane \
-    master-local-ip &
+    openstack port create \
+        --fixed-ip ip-address=192.168.24.2 \
+        --network jslagle-master-ctlplane \
+        master-local-ip &
 
-openstack port create \
-    --fixed-ip ip-address=192.168.24.3 \
-    --network jslagle-master-ctlplane \
-    master-admin-host &
+    openstack port create \
+        --fixed-ip ip-address=192.168.24.3 \
+        --network jslagle-master-ctlplane \
+        master-admin-host &
 
-openstack port create \
-    --fixed-ip ip-address=192.168.24.4 \
-    --network jslagle-master-ctlplane \
-    master-public-host &
+    openstack port create \
+        --fixed-ip ip-address=192.168.24.4 \
+        --network jslagle-master-ctlplane \
+        master-public-host &
 
-openstack port create \
-    --fixed-ip ip-address=10.0.0.5 \
-    --network jslagle-master-external \
-    master-external-uc
+    openstack port create \
+        --fixed-ip ip-address=10.0.0.5 \
+        --network jslagle-master-external \
+        master-external-uc
+}
 
+function create_master_uc {
+    master_local_ip=$(openstack port show master-local-ip -f value -c id)
+    master_external_uc=$(openstack port show master-external-uc -f value -c id)
 
+    openstack server create \
+        --flavor ocp-master-large \
+        --image CentOS-8-x86_64-GenericCloud-released-latest \
+        --key-name jslagle \
+        --nic net-id=57167586-7aef-4f82-aeb7-42f0ca71005f \
+        --nic port-id=$master_local_ip \
+        --nic port-id=$master_external_uc \
+        master-uc
 
-master_local_ip=$(openstack port show master-local-ip -f value -c id)
-master_external_uc=$(openstack port show master-external-uc -f value -c id)
-
-openstack server create \
-    --flavor ocp-master-large \
-    --image CentOS-8-x86_64-GenericCloud-released-latest \
-    --key-name jslagle \
-    --nic net-id=57167586-7aef-4f82-aeb7-42f0ca71005f \
-    --nic port-id=$master_local_ip \
-    --nic port-id=$master_external_uc \
-    master-uc
-
-openstack server add floating ip master-uc 10.0.126.102
+    openstack server add floating ip master-uc 10.0.126.102
+}
 
 function create {
     name=$1
@@ -163,24 +169,34 @@ function delete {
 }
 
 
-for i in 0 1 2; do
-    ip=$((10 + $i))
-    create controller $ip $i ocp-master-large &
-done
+function create_controllers {
+    for i in 0 1 2; do
+        ip=$((10 + $i))
+        create controller $ip $i ocp-master-large &
+    done
+}
 
-for i in 0 1 2; do
-    ip=$((110 + $i))
-    create master-compute $ip $i m1.large &
-done
+function create_computes {
+    for i in 0 1 2; do
+        ip=$((110 + $i))
+        create master-compute $ip $i m1.large &
+    done
+}
 
-for i in 0 1 2; do
-    delete master-compute $i &
-done
+function delete_computes {
+    for i in 0 1 2; do
+        delete master-compute $i &
+    done
+}
 
-for i in 0 1 2; do
-    openstack server rebuild master-controller-${i} --image CentOS-8-x86_64-GenericCloud-released-latest &
-done
+function rebuild_controllers {
+    for i in 0 1 2; do
+        openstack server rebuild master-controller-${i} --image CentOS-8-x86_64-GenericCloud-released-latest &
+    done
+}
 
-for i in 0 1 2; do
-    openstack server rebuild master-compute-${i} --image CentOS-8-x86_64-GenericCloud-released-latest &
-done
+function rebuild_computes {
+    for i in 0 1 2; do
+        openstack server rebuild master-compute-${i} --image CentOS-8-x86_64-GenericCloud-released-latest &
+    done
+}
