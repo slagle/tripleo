74fd6febc09cb824eafa7538be483bc841b38ed4
James Slagle <jslagle@redhat.com>
2021-04-14 16:48:25 -0400

    updates

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -75,8 +75,8 @@ if [ "$HEATCLIENT" = "0" ]; then
                 -n $NETWORK_DATA \
                 $ARGS \
                 $ENVIRONMENTS \
-                $@ 2>&1 | unbuffer -p tee -a deploy.log
-        date | tee -a deploy.log
+                $@ 2>&1 | tee -a deploy.log
+        date | unbuffer -p tee -a deploy.log
         echo "Ending deploy"
 else
 
