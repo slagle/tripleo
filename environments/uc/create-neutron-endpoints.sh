34b37854d3c2d92eb1f95fb8feb053c0f90cfdeb
James Slagle <jslagle@redhat.com>
2021-04-22 15:16:51 -0400

    updates

--- environments/uc/create-neutron-endpoints.sh
+++ environments/uc/create-neutron-endpoints.sh
@@ -4,5 +4,5 @@ set -eux
 
 openstack service create --name neutron network
 openstack endpoint create network admin http://192.168.122.56:9696 --region regionOne
-openstack endpoint create network public http://192.168.122.57:13696 --region regionOne
+openstack endpoint create network public https://192.168.122.57:13696 --region regionOne
 openstack endpoint create network internal http://192.168.122.56:9696 --region regionOne
