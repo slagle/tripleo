4ee0ee1c050cbb0b5c12e8f4f9bb962e2fc94286
James Slagle <jslagle@redhat.com>
2021-07-01 21:45:43 -0400

    updates

---
+++ environments/xena/standalone/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eux
+
+export IP=192.168.24.2
+export VIP=192.168.24.3
+export NETMASK=24
+export INTERFACE=eth1
+
+sudo openstack tripleo deploy \
+    --templates \
+    --local-ip=$IP/$NETMASK \
+    --control-virtual-ip $VIP \
+    -e /usr/share/openstack-tripleo-heat-templates/environments/standalone/standalone-tripleo.yaml \
+    -r /usr/share/openstack-tripleo-heat-templates/roles/Standalone.yaml \
+    -e $HOME/containers-prepare-parameters.yaml \
+    -e $HOME/standalone_parameters.yaml \
+    --standalone
---
+++ scripts/add-eth1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cat >/etc/systemd/system/add-eth1.service<<EOF
+
+EOF
