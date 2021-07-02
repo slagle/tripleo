474175297f5d7bbd97339aa84d14b88c2abc6a19
James Slagle <jslagle@redhat.com>
2021-07-01 22:09:28 -0400

    updates

--- scripts/add-eth1.sh
+++ scripts/add-eth1.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-cat >/etc/systemd/system/add-eth1.service<<EOF
+sudo /bin/bash -c "cat >/etc/systemd/system/add-eth1.service<<EOF
+[Unit]
+After=local-fs.target
 
+[Service]
+Type=oneshot
+ExecStart=/bin/sh -c 'ip link add dev eth1 type dummy && ip link set dev eth1 up'
 EOF
+"
+
+sudo systemctl daemon-reload
+sudo systemctl restart add-eth1
+
