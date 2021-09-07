2074ef44118ee498a1d099f23521605db6987c6f
James Slagle <jslagle@redhat.com>
2021-09-07 19:47:19 -0400

    updates

--- environments/uc-wallaby/undercloud.sh
+++ environments/uc-wallaby/undercloud.sh
@@ -12,28 +12,31 @@ sudo /usr/local/bin/tripleo-repos current-tripleo-dev
 
 sudo dnf -y install git bash-completion tmux python3-tripleoclient expect tripleo-ansible libibverbs
 
-cat >~/.tmux.conf<<EOF
+if [ ! -f ~/.tmux.conf ]; then
+    cat >~/.tmux.conf<<EOF
 # Prefix key
 set-option -g prefix2 C-a
 bind-key C-a send-prefix -2
 bind-key T command-prompt -p "title:" "set set-titles-string %1"
 set-option -g set-titles on
 EOF
+fi
 
 tmux new-session -d -s undercloud
 
 tmux send-keys -t undercloud:0 '
-cp tripleo/environments/uc/undercloud.conf ~/undercloud.conf
-cp tripleo/environments/uc/default-route.yaml ~
+cp tripleo/environments/uc-wallaby/undercloud.conf ~/undercloud.conf
 cp -a /usr/share/openstack-tripleo-heat-templates tripleo-heat-templates
-sudo dd if=/dev/zero of=/swapfile bs=1M count=8192
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-sudo hostnamectl set-hostname $UC_HOSTNAME
-sudo tee -a /etc/fstab << EOF
+if [ ! -f /swapfile ]; then
+    sudo dd if=/dev/zero of=/swapfile bs=1M count=8192
+    sudo chmod 600 /swapfile
+    sudo mkswap /swapfile
+    sudo swapon /swapfile
+    sudo tee -a /etc/fstab << EOF
 /swapfile swap swap defaults 0 0
 EOF
+fi
+sudo hostnamectl set-hostname $UC_HOSTNAME
 openstack undercloud install
 echo "source ~/stackrc" >> ~/.bashrc
 openstack complete | grep -v osc_lib | sudo tee /etc/bash_completion.d/openstack
