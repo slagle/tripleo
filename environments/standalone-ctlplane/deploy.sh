4f843045feb74128a76b28d2d22193c869b7e6ae
James Slagle <jslagle@redhat.com>
2022-06-28 20:40:36 -0400

    updates

--- environments/standalone-ctlplane/deploy.sh
+++ environments/standalone-ctlplane/deploy.sh
@@ -7,4 +7,4 @@ SCRIPT_DIR=$(realpath $(dirname $0))
 export ROLES_FILE="$HOME/tripleo-heat-templates/roles/Controller.yaml"
 export ROLE="Controller"
 
-${SCRIPT_DIR}/../standalone-master/deploy.sh
+${SCRIPT_DIR}/../standalone-master/deploy.sh -e ${SCRIPT_DIR}/standalone-ctlplane-parameters.yaml
